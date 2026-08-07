fdecbf2ab7a2ca62fec25b1237a810b9aa53e236
Xuan Yang <136431507+Aldoubt@users.noreply.github.com>
2026-08-07 15:50:36 +0800

    V25-05: make readiness check a stateful BT node

--- src/agt_bt_executor/include/agt_bt_executor/is_task_ready.hpp
+++ src/agt_bt_executor/include/agt_bt_executor/is_task_ready.hpp
@@ -1,15 +1,29 @@
 #pragma once
-#include <behaviortree_cpp/condition_node.h>
+
+#include <behaviortree_cpp/action_node.h>
 #include <agt_interfaces/srv/evaluate_task_readiness.hpp>
 #include <rclcpp/rclcpp.hpp>
+
+#include <chrono>
+
 namespace agt_bt_executor {
-class IsTaskReady final : public BT::ConditionNode {
+
+class IsTaskReady final : public BT::StatefulActionNode
+{
 public:
-  IsTaskReady(const std::string &, const BT::NodeConfiguration &, rclcpp::Node::SharedPtr);
+  IsTaskReady(
+    const std::string &, const BT::NodeConfiguration &, rclcpp::Node::SharedPtr);
+
   static BT::PortsList providedPorts();
-  BT::NodeStatus tick() override;
-private: rclcpp::Node::SharedPtr node_; rclcpp::Client<agt_interfaces::srv::EvaluateTaskReadiness>::SharedPtr client_;
+  BT::NodeStatus onStart() override;
+  BT::NodeStatus onRunning() override;
+  void onHalted() override;
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Client<agt_interfaces::srv::EvaluateTaskReadiness>::SharedPtr client_;
   rclcpp::Client<agt_interfaces::srv::EvaluateTaskReadiness>::SharedFuture future_;
-  rclcpp::Time started_;
+  std::chrono::steady_clock::time_point started_{};
 };
-}
+
+}  // namespace agt_bt_executor
