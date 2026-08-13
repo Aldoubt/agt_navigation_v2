e0adc4e6807d278dda5e01520d351372e0f2409f
Xuan Yang <136431507+Aldoubt@users.noreply.github.com>
2026-08-13 17:07:48 +0800

    feat(v25-12c): export map frame calibration primitives

--- src/agt_map_workbench/agt_map_workbench/__init__.py
+++ src/agt_map_workbench/agt_map_workbench/__init__.py
@@ -1,5 +1,24 @@
 """Offline AGT Map Workbench primitives."""
 
-from .model import WorkbenchRecipeModel, WorkbenchOperation
+from .frame_calibration import (
+    MAP_FRAME_SCHEMA,
+    AxisFit,
+    MapFrameCalibration,
+    fit_horizontal_axis_from_corridor,
+    fit_vertical_axis_from_cylinder,
+    nearest_xyz_in_window,
+    solve_map_frame,
+)
+from .model import WorkbenchOperation, WorkbenchRecipeModel
 
-__all__ = ["WorkbenchRecipeModel", "WorkbenchOperation"]
+__all__ = [
+    "MAP_FRAME_SCHEMA",
+    "AxisFit",
+    "MapFrameCalibration",
+    "WorkbenchOperation",
+    "WorkbenchRecipeModel",
+    "fit_horizontal_axis_from_corridor",
+    "fit_vertical_axis_from_cylinder",
+    "nearest_xyz_in_window",
+    "solve_map_frame",
+]
