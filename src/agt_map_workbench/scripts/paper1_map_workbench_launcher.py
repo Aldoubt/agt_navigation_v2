ab8a817846c4efc8774c35b9fccf101d2d37394d
Xuan Yang <xuanyang.robotics@gmail.com>
2026-08-23 08:06:39 +0800

    feat(map): launch controlled ablation workbench

--- src/agt_map_workbench/scripts/paper1_map_workbench_launcher.py
+++ src/agt_map_workbench/scripts/paper1_map_workbench_launcher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from agt_map_workbench.review_export_workbench import ReviewExportMapWorkbenchWindow, main
+from agt_map_workbench.ablation_workbench import AblationMapWorkbenchWindow, main
 
 
 if __name__ == "__main__":
